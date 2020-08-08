ea6b415c2f7e404cb2841bf2d92f624bc1757448
Franz Bascope <franzbascope123@gmail.com>
2020-08-08 10:35:08 -0400

    reconnect db after fail get products test

--- src/routes/__tests__/products.test.js
+++ src/routes/__tests__/products.test.js
@@ -60,5 +60,6 @@ describe("Products Router Test", () => {
     const res = await request(app).get("/products").send();
     expect(res.statusCode).toEqual(500);
     expect(res.body.message).toBeDefined();
+    connectMongo();
   });
 });
